5d80d071c8ba3df89a2ff907a64690e352c924dc
natalieronson <natalie.ronson@gmail.com>
2018-02-15 14:22:29 -0500

    Retrieving user input and selecting information from TicketMaster

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,72 +22,137 @@ myApp.getTicketMasterEvents = (userLocation, userStartDate, userEndDate) => {
                 
             }
         }).then(function(res){
+                myApp.getEventInfo(res);
+        });
+}
+
+myApp.getEventInfo = function(res) {
                 const events = res._embedded.events;
 
                 console.log(events);
 
                 console.log(events[0].name)
 
-                let displayedEventInfo = events
+                let eventInfo = events
                     .filter(item => item._embedded.attractions !== undefined)
-                    .map(function(item){
+                    .map(function (item) {
                         return {
                             name: item._embedded.attractions[0].name,
                             startDate: item.dates.start.localDate,
-                            url: item.url, 
+                            url: item.url,
                             venue: item._embedded.venues[0].name
                         }
                     })
-                console.log(displayedEventInfo);
+                console.log(eventInfo);
+                myApp.setupSpotify(eventInfo)
+                // myApp.getArtistID(eventInfo);
 
-            });
+            
 }
 
 // //spotify API
-// let headers = {};
+let headers = {};
 
-// $.ajax({
-//     url: "http://proxy.hackeryou.com",
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json',
-//         'Accept': 'application/json'
-//     },
-//     data: JSON.stringify({
-//         reqUrl: 'https://accounts.spotify.com/api/token',
-//         params: {
-//             grant_type: 'client_credentials'
-//         },
-//         proxyHeaders: {
-//             "Authorization": "Basic YzExYWIyNTAxYjA0NDg2YjhlN2M2MzJjYmIxM2M1NjQ6ODBkZDZkZWUwYWMyNGZiNzk2YTc3MDcwYWY3NmMxMjY="
-//         }
-//     })
-// }).then(function (res) {
-//     // console.log(res);
-//     headers = {
-//         "Authorization": res.token_type + " " + res.access_token
-//     }
+myApp.setupSpotify = function(eventInfo) {
+    $.ajax({
+        url: "http://proxy.hackeryou.com",
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        data: JSON.stringify({
+            reqUrl: 'https://accounts.spotify.com/api/token',
+            params: {
+                grant_type: 'client_credentials'
+            },
+            proxyHeaders: {
+                "Authorization": "Basic YzExYWIyNTAxYjA0NDg2YjhlN2M2MzJjYmIxM2M1NjQ6ODBkZDZkZWUwYWMyNGZiNzk2YTc3MDcwYWY3NmMxMjY="
+            }
+        })
+    }).then(function (res) {
+        // console.log(res);
+        headers = {
+            "Authorization": res.token_type + " " + res.access_token
+        }
+        myApp.getArtistID(eventInfo);
+    });
+      
+}
+
+myApp.getArtistID = function(eventInfo) {
+
+            console.log(eventInfo);
+
+            const getResponses = function(artist) {
+                let url = "https://api.spotify.com/v1/";
+               return $.ajax({
+                   url: url + "search",
+                   method: "GET",
+                   headers: headers,
+                   dataType: "json",
+                   data: {
+                       type: "artist",
+                       q: artist.name,
+                       limit: 1
+                   }
+                });
+            }
+
+            const responses = [];
+            eventInfo.forEach(function(item){
+                responses.push(getResponses(item));
+            });
+
+            $.when(...responses) 
+                .then((...args) => {
+                    
+                   args = args.map(arg => arg[0].artists.items);
+                   args.filter(item => item.length > 0);
+                    console.log(args);
+                        
+                })
+}
 
 
 
-//     let url = "https://api.spotify.com/v1/";
+   
+             
+        //        let url = "https://api.spotify.com/v1/";
+        //        $.ajax({
+        //            url: url + "search",
+        //            method: "GET",
+        //            headers: headers,
+        //            dataType: "json",
+        //            data: {
+        //                type: "artist",
+        //                q: artist.name,
+        //                limit: 1
+        //            }
+        //        }).then(function (res) {
+        //        });
+              
 
+        //    }).then(function(){
 
-//     $.ajax({
-//         url: url + "search",
-//         method: "GET",
-//         headers: headers,
-//         dataType: "json",
-//         data: {
-//             type: "artist",
-//             q: "Drake"
-//         }
-//     }).then(function (res) {
-//         console.log(res);
-//     });
+        //    })
+        // artistsWithIDs = artistsWithIDs.filter(artist => {
+        //     artist.artists.items.length !== 0;
+        // });
+
+//         console.log(artistsWithIDs);
+// }
+
+// eventInfo.forEach(function (artist) {
+    // console.log(artist.name);
 
 
 // });
+
+
+
+
+    
 
 
 
